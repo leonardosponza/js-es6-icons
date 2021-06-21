1839b6d846abd2d3a0a3c19b240cd17d70f37432
leonardo <sponzal4@gmail.com>
2021-06-21 18:00:51 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -97,72 +97,25 @@ const icons = [
     },
   ];
 
-//   milestone1
-
-function print(array){
-
-  array.forEach((element) => {
-    console.log(element);
-
-    const {name, prefix, family} = element;
-
-    const elementHTML = `
-    <div>
-      <i class="${family} ${prefix}${name}"></i>
-      <div class="title">${name.toUpperCase()}</div>
-    </div>
-    `;
-
-    document.getElementById('icons').innerHTML += elementHTML;
+  print(icons);
   
-  });
-
- }
-    // milestone2
-
-function coloreicone(array){
-    const color = icons.map((element) => {
-        let color = 'purple'
-        if(element.type == 'animal'){
-            color = 'blue'
-        } else if (element.type == 'vegetable'){
-            color = 'green'
-        }
-
-
-        return {
-           ...element,
-           color
-        }
-
+  /*----- funzioni ---- */
+   function print(array){
+  
+    array.forEach((element) => {
+      console.log(element);
+  
+      const {name, prefix, family} = element;
+  
+      const elementHTML = `
+      <div>
+        <i class="${family} ${prefix}${name}"></i>
+        <div class="title">${name.toUpperCase()}</div>
+      </div>
+      `;
+  
+      document.getElementById('icons').innerHTML += elementHTML;
+    
     });
-    return color
-}
-
-const types = [];
-
-// milestone3
-
-let typeIcons = [];
-colorIcon.forEach((element) => {
-    if(!typeIcons.includes(element.type)){
-        typeIcons.push(element.type);
-        document.getElementById('filter-select').innerHTML += 
-        `<option value=${element.type}>${element.type}</option>`;
-    }
-});
-
-console.log(typeIcons);
-
-
-filterType.addEventListener("change", function() {
-    let iconsShow;
-
-    iconsShow = colorIcon;
-    if(this.value != 'all'){
-        iconsShow = colorIcon.filter((element) => element.type == this.value);
-    }
-
-    resetHtml();
-    printColorIcon(iconsShow);
-});
+  
+   }
